34345865595ebc03350f70c7ee365918ca05f181
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2026-08-29 09:49:32 +0530

    chore: add solar icons for react and next

--- apps/landing/src/shared/components/navbar/navbar.tsx
+++ apps/landing/src/shared/components/navbar/navbar.tsx
@@ -9,12 +9,12 @@ const navLinks = [
 ];
 
 const navLinkClassName =
-  "px-2 py-1 text-sm font-medium transition-colors text-fg-3 hover:text-fg-4";
+  "px-2 py-1 text-md font-medium transition-colors text-fg-3 hover:text-fg-4";
 
 export function Navbar() {
   return (
     <header className="fixed top-8 left-1/2 z-99 hidden w-[530px] -translate-x-1/2 overflow-hidden rounded-[24px] bg-background shadow-xs backdrop-blur-lg md:block">
-      <nav className="flex h-15 items-center justify-between gap-2 px-4">
+      <nav className="flex items-center justify-between gap-2 p-4">
         <div className="flex items-center gap-5">
           <Link href="/" className="shrink-0 text-fg-4">
             <Logo />
