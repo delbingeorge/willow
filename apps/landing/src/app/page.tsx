c73a7c8ef435b67eaa9534d2640677f8b3155817
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2026-08-29 08:42:10 +0530

    config: add tailwind to web/ just like landing/ and add welcome styles

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Welcome to Willow Pages!</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl">Welcome to Willow Pages!</h1>
+    </div>
+  );
 }
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>Welcome to Willow Pages!</>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl">Welcome to Willow Pages!</h1>
+    </div>
+  );
 }
 
 export default App;
